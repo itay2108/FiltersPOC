c170f4562744e5935c61c0354d4bf28bc07a543b
Itay Gervash <gervash@icloud.com>
2023-02-12 22:23:38 +0200

    Initial Commit

---
+++ FiltersPOC/FiltersPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiltersPOCApp.swift
+//  FiltersPOC
+//
+//  Created by Itay Gervash on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FiltersPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
